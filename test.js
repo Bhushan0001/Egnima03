b4f14bba5612785f5fa81b360c8e08a527ed4f5d
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-16 00:03:34 +0530

    datatypes

---
+++ test.js
@@ -0,0 +1,14 @@
+console.log("Happy Coding journey")
+const accountId = 133434;
+let accountEmail = "bhushan@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+accountEmail = "abc@gmail.com"
+accountPassword = "2121212"
+accountCity = "nashik"
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+console.log(typeof undefined)
+console.log(typeof null)
