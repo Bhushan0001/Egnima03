6c113bb2c0ac91944587bd489b93958425d172f1
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-16 19:33:33 +0530

    Math & number

---
+++ 06nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400;  
+
+const balance = new Number(100);
+console.log(balance.toString().length)
+
+const hundreds = 1234545455.101
+console.log(hundreds.toLocaleString('en-IN')) // this method show the values in indian standards
+
+
+//++++++++++++++++++++++++++++++++  Math ++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-3))
+console.log(Math.round(35.3))
+console.log(Math.round(4.5))
+console.log(Math.floor(4.4))
+console.log(Math.ceil(45.4))
+console.log((Math.random()))
