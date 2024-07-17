e542a8a2a036af5853d38158dc6d5b3e6627589e
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-17 12:32:45 +0530

    difference between splice and slice

---
+++ 01_arrays.js
@@ -0,0 +1,28 @@
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr)
+
+// const myHeoros =["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1,2,3,4)
+// // console.log(myArr2[1])
+// myArr2.push(6)
+// console.log(myArr2)
+// myArr2.pop()
+// console.log(myArr2)
+// myArr2.unshift(2)
+// console.log(myArr2)
+// console.log("shifting the element array")
+// myArr.shift()
+// console.log(myArr2)
+
+// Difference between slice & splice 
+
+const myArr1 = [1,2,3,4,5,6];
+
+const newArray = myArr1.slice(1,3);
+console.log(myArr1)
+console.log(newArray)
+
+const newarray2 = myArr1.splice(1,3);
+console.log(newarray2)
+console.log(myArr1)
---
+++ 07_datesinJs.js
@@ -0,0 +1,19 @@
+let myDate = new Date()
+
+console.log(myDate.toString())
+ 
+console.log(typeof myDate)
+
+// let myCreateDate = new Date(2023, 1, 23)
+// console.log(myCreateDate.toString())
+
+let myCreateDate = new Date("02-01-2024")
+// console.log(myCreateDate.toLocaleString())
+// console.log(myCreateDate.toLocaleDateString)
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+console.log(myCreateDate.getTime())
+console.log(Date.now())
