20bbbcb8c60fdf127a14157f4ce0751c827d2a11
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-19 19:20:18 +0530

    learn about Scope eg. global , local , block scope

---
+++ ScopesJS/01_scope.js
@@ -0,0 +1,43 @@
+
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    
+    two() 
+    // console.log(website)
+
+}
+// one()
+
+// if(true){
+//     const username = "hitesh"
+//     if(username === "hitesh"){
+//         const website = " youtube"
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+// }
+
+// console.log(username)
+
+// ++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+// addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
+
+
+
+// const addthree = (num)=> {
+//     return num+ 3
+// }
+
+// addthree(5)
