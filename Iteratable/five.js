999a6a3cb249066513c1b05004415aed43048596
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-21 23:08:10 +0530

    mend

--- Iteratable/five.js
+++ Iteratable/five.js
@@ -47,6 +47,15 @@ coding.forEach((item) =>{
 
 const myNums = [1,2,3,4,5,6,7,8,9,10]
    const nums = myNums.filter( (num) =>{
-    num>4
+   return  num>4
    } )
-   console.log(nums)
+//    console.log(nums)
+
+   const newNums = []
+   myNums.forEach((num)=>{
+    if(num>4){
+        newNums.push(num)
+    }
+   })
+
+//    console.log(newNums)
