7dd69d584503b05380b2adb95a0c334958526f7d
Bhushan <bhushanharpadeo8@gmail.com>
2024-07-21 22:51:42 +0530

    Learn Higher order functions ex foreach , map filter

---
+++ Iteratable/five.js
@@ -0,0 +1,52 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// coding.forEach(function (val){
+//     console.log(val)
+// })
+
+// coding.forEach( (val)=>{
+//     console.log(val)
+// })
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item , index , arr)=>{
+//     console.log(item, index, arr)
+// })
+
+// const myCoding = [
+//     {
+//         languageName : "javascript", 
+//         languageFileName : "js"
+//     },
+//     {
+//         languageName : "java", 
+//         languageFileName : "java"
+//     },
+//     {
+//         languageName : "python", 
+//         languageFileName : "py"
+//     }
+// ]
+
+// myCoding.forEach( (val) => {
+//     console.log(`${val["languageFileName"]} := ${val.languageFileName} `)
+// } )
+
+
+coding.forEach((item) =>{
+    console.log(item)
+})
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+   const nums = myNums.filter( (num) =>{
+    num>4
+   } )
+   console.log(nums)
